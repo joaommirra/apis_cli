e1d98539992aca794efde4ca86afef6af8c5eb5d
Joao Mirra <joaommirra@outlook.com>
2021-03-07 10:12:37 -0300

    update

--- apis_cli.py
+++ apis_cli.py
@@ -4,5 +4,11 @@ import click
 def apis():
     """A CLI wrapper for the API of Public APIs."""
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     apis(prog_name='apis')
+
+@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
+@apis.command()
+def entries(no_auth: bool):
+    """List all cataloged APIs."""
+
